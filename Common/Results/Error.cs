b84e1350e2cfde510445376e8c7763edc8d8bd3f
NelsonDavid59 <fleitasnelson59@gmail.com>
2025-11-18 11:34:41 -0300

    Clases para gestión de Result Pattern

--- Common/Results/Error.cs
+++ Common/Results/Error.cs
@@ -4,7 +4,13 @@
 
     public static class Errors
     {
-        public static Error RscNotFound { get; } = new("RscNotFound", ErrorType.NotFound, "Recurso no encontrado.");
-        public static Error UnauthorizedUser { get; } = new("UnauthorizedUser", ErrorType.Unauthorized, "Usuario no autorizado.");
+        public static Error RscNotFound { get; } = 
+            new("RscNotFound", ErrorType.NotFound, "Recurso no encontrado.");
+        
+        public static Error UnauthorizedUser { get; } = 
+            new("UnauthorizedUser", ErrorType.Unauthorized, "Usuario no autorizado.");
+
+        public static Error InternalServerError { get; } = 
+            new("InternalServerError", ErrorType.Internal, "Error interno en el servidor.");
     }
 }
--- Common/Results/ErrorType.cs
+++ Common/Results/ErrorType.cs
@@ -3,6 +3,7 @@
     public enum ErrorType
     {
         NotFound,
-        Unauthorized
+        Unauthorized,
+        Internal
     }
 }
