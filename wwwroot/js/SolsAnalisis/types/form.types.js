ca166a6b16f49cf2ee80bd4a57dd4b13127d0068
NelsonDavid59 <fleitasnelson59@gmail.com>
2025-11-21 10:57:57 -0300

    Definición de archivo JSDoc para tipo de elementos del DOM para el formulario de Solicitud de Análisis

---
+++ wwwroot/js/SolsAnalisis/types/form.types.js
@@ -0,0 +1,7 @@
+﻿/**
+ * @typedef {object} DOMElements
+ * @property {HTMLFormElement} form Elemento form del Solicitud de Análisis
+ * @property {HTMLInputElement} buscadorClienteInp Elemento input Buscador de clientes
+ * @property {HTMLTableElement} tableElem Elemento table del formulario (Detalles de Análisis)
+ * @property {HTMLTemplateElement} rowTemplateElem Elemento template - Fila para agregarse 
+ */
